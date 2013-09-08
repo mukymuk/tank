cfec144f426dd5dc3e8f2445bcc871b5a90b4764
Shawn Brooks <mukymuk@gmail.com>
2013-09-08 17:41:33 -0500

    sumpcirc

---
+++ sumpcirc.js
@@ -0,0 +1,28 @@
+var rb = require('./rb');
+var log = require('./log').log;
+
+var right = rb.open(1,6);
+var left = rb.open(1,7);
+
+
+cycle = function()
+{
+	if( right.get() )
+	{
+		log("sumpcirc, right, off");
+		right.set(false);
+		log("sumpcirc, left, on");
+		left.set(true);
+	}
+	else
+	{
+		log("sumpcirc, left, off");
+		left.set(false);
+		log("sumpcirc, right, on");
+		right.set(true);
+	}
+}
+
+cycle();
+setInterval( cycle, 3 * 60 * 60 * 1000 );	// 3 hours
+
