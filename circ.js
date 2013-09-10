854801cd5b415e77c82e47c64a2410ae3b678cb5
Shawn Brooks <mukymuk@gmail.com>
2013-09-09 22:17:33 -0500

    circ

---
+++ circ.js
@@ -0,0 +1,36 @@
+var execFile = require('child_process').execFile;
+var log = require('./log').log;
+
+var flowTime = 20 * 1000;
+var flowHz = 59.9;
+
+var ebbTime	= 30 * 1000;
+var ebbHz = 30.3;
+
+
+ebb = function()
+{
+	log("circ, %d", ebbHz );
+	execFile("./x200", [ ebbHz ] );
+}
+
+flow = function()
+{
+	log("circ, %d", flowHz );
+	execFile("./x200", [ flowHz ] );
+	setTimeout( ebb, flowTime );
+}
+
+init = function()
+{
+	setInterval( flow, flowTime+ebbTime );
+	flow();
+}
+
+init();
+
+module.exports =
+{
+	init: init
+};
+
